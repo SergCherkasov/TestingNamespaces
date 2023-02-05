35fb2034660b36ba5ce939b84987f81690afaa47
Serg Cherkasov <serg.cherkasov@gmail.com>
2023-02-05 11:15:21 +0200

    Connecting UI with main

--- TestingNamespaces/Program.cs
+++ TestingNamespaces/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace TestingNamespaces
+﻿using TestingNamespaces.UI;
+
+namespace TestingNamespaces
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            UI.ui ui=new UI.ui();
+
+            ui.Run();
         }
     }
 }
