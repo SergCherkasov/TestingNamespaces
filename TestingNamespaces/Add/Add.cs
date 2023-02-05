b7132dbe7eb86e571bcdbad060e244ba741c0158
Serg Cherkasov <serg.cherkasov@gmail.com>
2023-02-05 10:54:37 +0200

    Class Add

---
+++ TestingNamespaces/Add/Add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingNamespaces.Add
+{
+    public class Add
+    {
+        public int Adding(int x, int y) { return x + y; }
+    }
+}
