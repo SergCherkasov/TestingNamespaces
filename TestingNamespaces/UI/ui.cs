00a7d1e8310a684d843fb0149907b9deb774e925
Serg Cherkasov <serg.cherkasov@gmail.com>
2023-02-05 11:09:20 +0200

    Class UI

---
+++ TestingNamespaces/UI/ui.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingNamespaces.Add;
+
+namespace TestingNamespaces.UI
+{
+    public class ui
+    {
+        TestingNamespaces.Add.Add add = new TestingNamespaces.Add.Add();
+
+        public void Run()
+        {
+            Console.WriteLine("Enter the nubers to ADD:");
+            Console.WriteLine();
+            Console.WriteLine("Enter X:");
+            int x =Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y:");
+            int y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int result = add.Adding(x, y);
+            Console.WriteLine($"Result of your expression:{result}");
+        }
+
+    }
+}
